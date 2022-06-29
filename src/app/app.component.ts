15ad2ac34d320435a5b457e20bc420a0b05d6b0a
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-06-29 20:33:06 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  [x: string]: any;
+  @ViewChild('videoPlayer', { static: false })
+  videoPlayer!: ElementRef;
+  isPlay: boolean = false;
+  toggleVideo() {
+    this['videoplayer'].nativeElement.play();
+  }
   title = 'angular-app';
 }
